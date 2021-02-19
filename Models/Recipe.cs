53412a0747370659eccec1e06d2ce2ea13677ffe
JordanJStone <statictrack@gmail.com>
2021-02-19 16:57:28 -0700

    EOD, created Recipe model

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -1,7 +1,13 @@
 namespace allSpice.Models
 {
-    public class Recipe
-    {
-        
-    }
+  public class Recipe
+  {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string CookTime { get; set; }
+  }
 }
+
+// What does a recipe have?
+// Id, Title, Description, Cook time
